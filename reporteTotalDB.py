1a419cdd95fa9085ad53ddadb8d6b36cb4d46fe6
meryje1995-gif <meryje1995@gmail.com>
2026-08-12 07:38:04 -0600

    Create reporteTotalDB.py

---
+++ reporteTotalDB.py
@@ -0,0 +1,8 @@
+'''
+Este archivo estaba vacío en el proyecto original (lo confirmé al
+revisar el que subiste). Se deja aquí tal cual para que el
+"from reporteTotalDB import*" de mantenimiento_v_1.py no truene.
+
+Aquí puede ir, por ejemplo, la función para la opción 4 del menú
+("Reporte Total Clientes").
+'''
